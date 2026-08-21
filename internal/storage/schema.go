b8b06678ccd31f4127a888105641d2ba13476124
ShivangNagta <shivangnag@gmail.com>
2026-08-21 13:08:30 +0530

    Simplify the schema

--- internal/storage/schema.go
+++ internal/storage/schema.go
@@ -1,19 +1,18 @@
+// Contains the initial DDL queries
+
 package storage
 
 var CreateLocalFilesTable = `
 	CREATE TABLE local_files (
 		id INTEGER PRIMARY KEY,
 		path TEXT NOT NULL UNIQUE,
-		size INTEGER,
-		mod_time DATETIME,
-		state TEXT NOT NULL,
-		updated_at DATETIME
+		state TEXT NOT NULL
 	)
 `
 
 var AddLocalFileTest = `
 	INSERT INTO local_files VALUES (
-		1, "./test", 1, 1, '1', 1
+		1, './test', '1'
 	)
 `
 
@@ -23,13 +22,12 @@ var CreatePendingOperationsTable = `
 		file_id INTEGER NOT NULL,
 		op_type TEXT NOT NULL,
 		status TEXT NOT NULL,
-		created_at DATETIME,
 		FOREIGN KEY (file_id) REFERENCES local_files(id)
 	)
 `
 
 var AddPendingOperationsTest = `
 	INSERT INTO pending_operations VALUES (
-		1, 1, '1', '1', 1
+		1, 1, '1', '1'
 	)
 `
